51081ace865954de0d9643316559839b0f1aa3a3
Nate Flood <nate@famconnect.net>
2015-01-07 14:25:18 -0500

    changed front page

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,12 +3,20 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
-      end
+    # div class: "blank_slate_container", id: "dashboard_default_message" do
+    #   span class: "blank_slate" do
+    #     span I18n.t("active_admin.dashboard_welcome.welcome")
+    #     small I18n.t("active_admin.dashboard_welcome.call_to_action")
+    #   end
+    # end
+    div class: "blank_slate_container", id: "main" do
+        span class: "blank_slate" do
+            span "Welcome to the ECHO Plant Information Workcenter"
+            small "For help with this system please contact Nate Flood at 239-823-7316."
+        end
     end
+
+
 
     # Here is an example of a simple dashboard with columns and panels.
     #
